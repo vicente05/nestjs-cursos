c1f9134aac493ea62814d3f6a4a0b03552210a0d
Vicente <vicentemline@gmail.com>
2021-05-01 13:50:16 +0200

    format del proyecto

--- src/api/products/tests/brand.service.spec.ts
+++ src/api/products/tests/brand.service.spec.ts
@@ -2,17 +2,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { BrandService } from './brand.service';
 
 describe('BrandService', () => {
-  let service: BrandService;
+    let service: BrandService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [BrandService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [BrandService],
+        }).compile();
 
-    service = module.get<BrandService>(BrandService);
-  });
+        service = module.get<BrandService>(BrandService);
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
 });
--- src/api/users/tests/customers.controller.spec.ts
+++ src/api/users/tests/customers.controller.spec.ts
@@ -2,17 +2,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { CustomersController } from './customers.controller';
 
 describe('CustomersController', () => {
-  let controller: CustomersController;
+    let controller: CustomersController;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [CustomersController],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            controllers: [CustomersController],
+        }).compile();
 
-    controller = module.get<CustomersController>(CustomersController);
-  });
+        controller = module.get<CustomersController>(CustomersController);
+    });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(controller).toBeDefined();
+    });
 });
